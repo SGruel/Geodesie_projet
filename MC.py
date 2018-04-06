8e35fbe2818a3219bb985dd797916ad6c77a7bf7
formation <formation@ubuntu.ubuntu-domain>
2018-04-06 09:34:57 +0200

    add MC

---
+++ MC.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on %(date)s
+
+@author: %(username)s
+"""
+
+#import re
+import numpy as np
+try:
+    import gpstime as gps
+except ImportError:
+    print("WARNING : gpstime non installé")
+
+#import math
+#import matplotlib.pyplot as plt
+#import skimage
+
+
+
+#Création des matrices
