76d9c0c27ee176c752eaf95b099edab1d6a57644
formation <formation@ubuntu.ubuntu-domain>
2018-04-06 11:58:30 +0200

    creation matrice Aet B

--- MC.py
+++ MC.py
@@ -15,7 +15,23 @@ except ImportError:
 #import math
 #import matplotlib.pyplot as plt
 #import skimage
-
-
+import parse_pos
 
 #Création des matrices
+def matriceB(list_pos):
+    mat_basline=np.zeros((len(list_base)+24,1))
+    j=0
+    for i in range (len(list_base)):
+        mat_basline[i]=list_base[i].X-list_base[i].X_o
+        mat_basline[i+1]=list_base[i].Y-list_base[i].Y_o
+        mat_basline[i+2]=list_base[i].Z-list_base[i].Z_o
+        if list_base[i].parent:
+            mat_baslin[-24+j]=list_base[i].X_o
+            j+=1
+            mat_baslin[-24+j]=list_base[i].Y_o
+            j+=1
+            mat_baslin[-24+j]=list_base[i].Z_o
+            j+=1
+
+def matrice_A(list_pos):
+    
